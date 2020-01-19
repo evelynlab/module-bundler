acfa6da2cf28f620f497af9f3b01832b146a3fa7
evelynlab <celayu@163.com>
2020-01-19 14:21:57 +0800

    chore: add webpack es6 demo

--- webpack-demo/01.basic/webpack.config.js
+++ webpack-demo/01.basic/webpack.config.js
@@ -3,10 +3,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+      'index': './src/index.js',
+      'index.es6': './src/index.es6.js'
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     devtool: 'source-map',
     mode: 'development',
